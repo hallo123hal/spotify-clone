1b3b7986174a01b00bc30d026903e4c8046e534c
Tung <sontungnguyen16@gmail.com>
2025-01-09 12:12:41 +0700

    Initial commit

---
+++ src/spotify.js
@@ -0,0 +1,5 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
