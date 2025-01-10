956427396d7323343487adf0d7a10a7f180ea921
Tung <sontungnguyen16@gmail.com>
2025-01-10 12:57:12 +0700

    authorized login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  //Run code based on a given condition
+  useEffect(() => {
+    const hash =  getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+    
+    console.log('I HAVE A TOKEN >>>', token);
+  }, []); //run once if none given here, if there is a variable, it will run everytime that variable changes
+
   return (
     <div className="app">
-
-      <Login />
-
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
